b28ddc2a476267f13f41fb6e3e19edde4fe36487
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-08 17:25:48 -0800

    added toLowerCase

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const searchBar = document.querySelector("#search-bar")
 
 searchBar.addEventListener("keyup", (e) => {
-  let search = e.target.value
+  let search = e.target.value.toLowerCase()
   console.log(search)
 })
